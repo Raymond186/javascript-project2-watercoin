c2ef8a34aac096e102fbc798bdca3d66790e3141
Raymond186 <raymondbeament@outlook.com>
2019-12-13 15:07:43 -0500

    logotwo-floating-now

---
+++ assets/Dombit.com Official Website!_files/script.js
@@ -0,0 +1,14 @@
+function toggle() {
+  // use jquery to select all the English elements
+  $(".english, .french").toggleClass("hide");
+  // use jquery to select all the french elements
+
+  // toggle the hide class on both english and french
+}
+
+// class to selector to show French language web site starting
+//use jquery
+$("button").on("click", toggle);
+// when web-page loads front page hide English show French only page
+
+//
